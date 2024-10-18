c0fa6e133a5fc205e67628958b9e1adc83f5d2c1
Alex A <146673386+al427RBLX@users.noreply.github.com>
2024-10-18 18:20:48 -0400

    Tested

--- script.js
+++
@@ -1,7 +0,0 @@
-const menuToggle = document.getElementById('mobile-menu');
-const navLinks = document.getElementById('nav-links');
-
-// Toggle menu visibility on click
-menuToggle.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
-});
