18ca01e61a766394da7c221942aabf590c012a7b
Alex A <146673386+al427RBLX@users.noreply.github.com>
2024-10-19 20:22:23 -0400

    Website Imrpvoed/Updated

---
+++ js/mobile-nav.js
@@ -0,0 +1,15 @@
+let headerMain = document.querySelector(".header-main");
+let headerMobileBtn  = document.querySelector(".mobile-menu-btn");
+
+let isMenuOpen = false;
+
+headerMobileBtn,onclick = function() {
+    if (! isMenuOpen) {
+        headerMain.style.display = "flex"
+        sMenuOpen = true;
+    }
+    if else (isMenuOpen) {
+        headerMain.style.display = "none"
+        sMenuOpen = false;
+    }
+}
