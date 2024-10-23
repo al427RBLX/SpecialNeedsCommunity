680d241c5ea6cfd58b2ab18504ddf920b06a633c
Alex A <146673386+al427RBLX@users.noreply.github.com>
2024-10-23 18:15:01 -0400

    Tested

--- js/test.js
+++
@@ -1,8 +0,0 @@
-function fitText(div) {
-    const text = div.textContent;
-    const fontSize = 16; // initial font size
-    while (getComputedStyle(div).width < div.scrollWidth) {
-      fontSize--;
-      div.style.fontSize = fontSize + 'px';
-    }
-  }
